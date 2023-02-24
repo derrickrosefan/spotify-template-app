287ad7e5c2b521d83a25b2f4a1ed2ee7f6835f37
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2023-02-24 12:12:08 -0600

    added styles to beautify home and login screen

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -13,15 +13,20 @@ const Home = () => {
     const [topSongs, isTopSongsReady] = useTopSongs();
     return (
         isTopSongsReady ?
-            <Container width="100%">
-                <Stack>
-                    <Row className="justify-content-center"><Col><h3 className="text-center pt-3">Your</h3></Col></Row>
-                    <Row className="justify-content-center"><Col><h1 className="text-center">Top 10</h1></Col></Row>
-                    <Row className="justify-content-center"><Col><h3 className="text-center p-1">Spotify Songs</h3></Col></Row>
-                    <Row className="justify-content-center"><Col xs={8}> <hr className="hr" /></Col></Row>
-                    <Row className="justify-content-center"><Col xs={8}>{topSongs.items.map(song => <SongCard key={song.id} song={song} />)}</Col></Row>
-                </Stack>
-            </Container>
+            <Container>
+                <Stack gap={2}>
+                    <Row className="justify-content-center text-center pt-3">
+                        <Col xs={8} style={{ "backgroundColor": "rgba(55, 207, 181, 0.60)", "borderRadius": "1.5em", "box-shadow": "0px 0px 1px 2px lightgrey" }} >
+                            <h3 className="pt-5 text-white">Your</h3>
+                            <h1 className="text-white">Top 10</h1>
+                            <h3 className="text-white">Spotify Songs</h3>
+                            <p className="pb-3 text-white">@derrickrosefan and @alison-park</p>
+                            <hr className="hr" />
+                            {topSongs.items.map(song => <SongCard key={song.id} song={song} />)}
+                        </Col>
+                    </Row>
+                </Stack >
+            </Container >
             : null
     );
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -12,13 +12,19 @@ const AUTH_URL =
 
 const Login = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>API University</h1>
-                    <Button variant="secondary" href={AUTH_URL}>Login!</Button>
+        <Container style={{ "height": "100vh" }}>
+            <Row className="align-items-center justify-content-center text-center p-5" style={{
+                "height": "100%"
+            }} >
+                <Col xs={5}>
+                    <div style={{ "backgroundColor": "rgba(55, 207, 181, 0.60)", "borderRadius": "1.5em", "box - shadow": "0px 0px 1px 2px lightgrey" }}>
+                        < h2 className="text-white pt-5">API University</h2>
+                        <Button variant="secondary mb-5" href={AUTH_URL}>Login!</Button>
+
+                    </div>
+
                 </Col>
-            </Row>
+            </Row >
         </Container >
     )
 }
